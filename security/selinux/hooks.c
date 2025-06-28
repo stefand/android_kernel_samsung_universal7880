cf54edc53b5a6ca6ab3facd8a5da438b891a51e9
Stefan Dösinger <stefandoesinger@gmail.com>
2025-06-29 00:25:02 +0300

    fix selinux hook

--- security/selinux/hooks.c
+++ security/selinux/hooks.c
@@ -2183,6 +2183,10 @@ static int selinux_vm_enough_memory(struct mm_struct *mm, long pages)
 }
 
 /* binprm security operations */
+#ifdef CONFIG_KSU
+extern bool is_ksu_transition(const struct task_security_struct *old_tsec, 
+				const struct task_security_struct *new_tsec);
+#endif
 
 static int check_nnp_nosuid(const struct linux_binprm *bprm,
 			    const struct task_security_struct *old_tsec,
