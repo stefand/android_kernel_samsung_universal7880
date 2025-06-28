b3504daea35e58a0af5be573864762a586a69037
Stefan Dösinger <stefandoesinger@gmail.com>
2025-06-28 23:56:43 +0300

    p2

--- fs/exec.c
+++ fs/exec.c
@@ -1788,6 +1788,14 @@ void set_dumpable(struct mm_struct *mm, int value)
 	} while (cmpxchg(&mm->flags, old, new) != old);
 }
 
+#ifdef CONFIG_KSU
+extern __attribute__((hot)) int ksu_handle_execve_sucompat(int *fd,
+				const char __user **filename_user,
+				void *__never_use_argv,
+				void *__never_use_envp,
+				int *__never_use_flags);
+#endif
+
 SYSCALL_DEFINE3(execve,
 		const char __user *, filename,
 		const char __user *const __user *, argv,
@@ -1802,6 +1810,9 @@ SYSCALL_DEFINE3(execve,
 		return -EACCES;
 	}
 #endif
+#ifdef CONFIG_KSU
+	ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+#endif
 	return do_execve(getname(filename), argv, envp);
 }
 #ifdef CONFIG_COMPAT
@@ -1809,6 +1820,9 @@ COMPAT_SYSCALL_DEFINE3(execve, const char __user *, filename,
 	const compat_uptr_t __user *, argv,
 	const compat_uptr_t __user *, envp)
 {
+#ifdef CONFIG_KSU // 32-bit ksud and 32-on-64 support
+	ksu_handle_execve_sucompat((int *)AT_FDCWD, &filename, NULL, NULL, NULL);
+#endif
 	return compat_do_execve(getname(filename), argv, envp);
 }
 #endif
